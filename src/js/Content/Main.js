9a59a34222fa78db28e1c288c4d2989b70c6bc70
olavokruel <olavokruel@gmail.com>
2019-10-15 20:27:00 -0300

    going to review section

--- src/js/Content/Main.js
+++ src/js/Content/Main.js
@@ -5,6 +5,11 @@ import Portugal2 from '../../assets/img/hotel-2.jpg';
 import Portugal3 from '../../assets/img/hotel-3.jpg';
 import IconStart from '../../assets/svg-icons/SVG/star.svg';
 import IconLocation from '../../assets/svg-icons/SVG/location-pin.svg';
+import Friend3 from '../../assets/img/user-3.jpg';
+import Friend4 from '../../assets/img/user-4.jpg';
+import Friend5 from '../../assets/img/user-5.jpg';
+import Friend6 from '../../assets/img/user-6.jpg';
+import Arrow from '../../assets/svg-icons/SVG/chevron-thin-right.svg';
 
 const Main = props => (
     <main className='hotel-view'>
@@ -37,6 +42,42 @@ const Main = props => (
                 <div className='overview__rating--average'>9.3</div>
                 <div className='overview__rating--count'>369 votes</div>
             </div>
+        </div>
+
+        <div className='detail'>
+            <div className='detail__description'>
+                <p className='paragraph'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Animi nisi dignissimos debitis ratione sapiente saepe.
+                    Accusantium cumque, quas, ut corporis incidunt deserunt
+                    quae architecto voluptate.Accusantium cumque, quas, ut
+                    corporis incidunt deserunt quae architecto voluptate
+                    delectus, inventore iure aliquid aliquam.
+                </p>
+                <p className='paragraph'>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga doloremque architecto dicta animi, totam, itaque officia ex.
+                </p>
+                <ul className='list'>
+                    <li className='list__item'><Arrow className='list__icon' viewBox="0 0 40 1" /> Close to the beach</li>
+                    <li className='list__item'><Arrow className='list__icon' viewBox="0 0 40 1"/>Breakfast included</li>
+                    <li className='list__item'><Arrow className='list__icon' viewBox="0 0 40 1"/>Free airport shuttle</li>
+                    <li className='list__item'><Arrow className='list__icon' viewBox="0 0 40 1"/>Free wi__fi in all rooms</li>
+                    <li className='list__item'><Arrow className='list__icon' viewBox="0 0 40 1"/>Air conditioning and heating</li>
+                    <li className='list__item'><Arrow className='list__icon' viewBox="0 0 40 1"/>Pets allowed</li>
+                    <li className='list__item'><Arrow className='list__icon' viewBox="0 0 40 1"/>We speak all languages</li>
+                    <li className='list__item'><Arrow className='list__icon' viewBox="0 0 40 1"/>Perfect for families</li>
+                </ul>
+                <div className='recommend'>
+                    <p className='recommend__count'>Lucy and 3 other friends recommend this hotel</p>
+                    <div className='recommend__friends'>
+                        <img src={Friend3} alt='Friend 1' className='recommend__photo'></img>
+                        <img src={Friend4} alt='Friend 2' className='recommend__photo'></img>
+                        <img src={Friend5} alt='Friend 3' className='recommend__photo'></img>
+                        <img src={Friend6} alt='Friend 4' className='recommend__photo'></img>
+                    </div>
+                </div>
+            </div>
+            <div className='detail__user-reviews'>User Reviews</div>
         </div>
     </main>
 )
